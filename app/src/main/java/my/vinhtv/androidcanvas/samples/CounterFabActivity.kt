8f9ea96602f76c9b2ab6798b9a0bf23fb4e0477e
mrThinBone <vinh.tv2003@gmail.com>
2018-05-23 17:06:09 +0700

    counter floating action activity

---
+++ app/src/main/java/my/vinhtv/androidcanvas/samples/CounterFabActivity.kt
@@ -0,0 +1,13 @@
+package my.vinhtv.androidcanvas.samples
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import my.vinhtv.androidcanvas.R
+
+class CounterFabActivity: AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_showcase_counter_fab)
+    }
+}
